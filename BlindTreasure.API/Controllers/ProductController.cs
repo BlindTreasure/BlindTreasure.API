e386a7c3327889221fbb6b82a98f82e0ba86b3c9
phuctran362003 <trangiaphuc362003181@gmail.com>
2025-04-30 11:14:25 +0700

    init api

---
+++ BlindTreasure.API/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using BlindTreasure.Application.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlindTreasure.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    // Ví dụ hardcoded cho đơn giản
+    private static readonly List<string> Products = new() { "Sneaker A", "Labubu B", "Anime C" };
+
+    [HttpGet("{id}")]
+    public IActionResult GetProduct(int id)
+    {
+        if (id < 0 || id >= Products.Count)
+        {
+            return BadRequest(ApiResult<string>.Failure("404", "Sản phẩm không tồn tại."));
+        }
+
+        var product = Products[id];
+        return Ok(ApiResult<string>.Success(product, "200", "Lấy thông tin sản phẩm thành công."));
+    }
+
+    [HttpGet("health-check")]
+    public IActionResult HealthCheck()
+    {
+        return Ok(ApiResult.Success("200", "Hệ thống hoạt động ổn định."));
+    }
+}
