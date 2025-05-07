1240ed4bfb81913c22828653ecd2435327e114b0
phuctran362003 <trangiaphuc362003181@gmail.com>
2025-05-07 09:48:10 +0700

    fix db

--- BlindTreasure.Domain/Entities/Role.cs
+++ BlindTreasure.Domain/Entities/Role.cs
@@ -1,10 +1,6 @@
 ﻿namespace BlindTreasure.Domain.Entities;
 
-public class Role : BaseEntity
+public class Role
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-
-    // 1-n: Role → Users
-    public ICollection<User> Users { get; set; }
+    
 }
