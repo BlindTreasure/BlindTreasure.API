905369f9d9f618e5ac871694e9e7fb2505b94809
Hoangtien1105 <minhtien11052003@gmail.com>
2025-05-17 01:48:15 +0700

    Basic CRUD for user service

---
+++ BlindTreasure.Domain/DTOs/UserDTOs/UserCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using BlindTreasure.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlindTreasure.Domain.DTOs.UserDTOs
+{
+    public class UserCreateDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? AvatarUrl { get; set; }
+        public RoleType RoleName { get; set; }
+    }
+}
---
+++ BlindTreasure.Domain/DTOs/UserDTOs/UserUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using BlindTreasure.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlindTreasure.Domain.DTOs.UserDTOs
+{
+    public class UserUpdateDto
+    {
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? AvatarUrl { get; set; }
+        public RoleType? RoleName { get; set; }
+        public UserStatus? Status { get; set; }
+    }
+}
