a9bb2534a911cad9d814d835257f938eafe4dbbd
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2025-05-22 12:35:11 +0700

    fix prod env

--- BlindTreasure.API/Program.cs
+++ BlindTreasure.API/Program.cs
@@ -47,7 +47,7 @@ var app = builder.Build();
 app.UseCors("AllowAll");
 //
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
