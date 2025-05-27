bbd4e4295b580837ceb88962d707dc360e8eb207
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2025-05-27 20:11:09 +0700

    authorize endpoint

--- BlindTreasure.Domain/DTOs/AuthenDTOs/SellerRegistrationDto.cs
+++ BlindTreasure.Domain/DTOs/AuthenDTOs/SellerRegistrationDto.cs
@@ -1,9 +1,14 @@
-﻿namespace BlindTreasure.Domain.DTOs.AuthenDTOs;
+﻿using System.ComponentModel;
+
+namespace BlindTreasure.Domain.DTOs.AuthenDTOs;
 
 public class SellerRegistrationDto
 {
+    [DefaultValue("trangiaphuc362003181@gmail.com")]
     public required string Email { get; set; }
+    [DefaultValue("trangiaphuc362003181@gmail.com")]
     public required string Password { get; set; }
+    [DefaultValue("trangiaphuc362003181@gmail.com")]
     public required string FullName { get; set; }
     public string? PhoneNumber { get; set; }
     public DateTime DateOfBirth { get; set; }
