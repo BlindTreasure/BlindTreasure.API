b2d8acde106857f2a89a635d5c1442c56be21113
nhatquang1605 <honhatquang1605@gmail.com>
2025-07-08 18:22:45 +0700

    Api participant/withdraw to promotion, change logic get all promotions

---
+++ BlindTreasure.Domain/DTOs/PromotionDTOs/ParticipantPromotionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlindTreasure.Domain.DTOs.PromotionDTOs
+{
+    public class ParticipantPromotionDto
+    {
+        public Guid Id { get; set; }
+        public Guid PromotionId { get; set; }
+        public Guid SellerId { get; set; }
+        public DateTime JoinedAt { get; set; }
+    }
+}
