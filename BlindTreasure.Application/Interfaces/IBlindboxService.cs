540fac6de548a685d69b1c6f39dc431140598cc4
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2025-07-27 20:09:11 +0700

    feat: Implement unit tests and swagger customization

--- BlindTreasure.Application/Interfaces/IBlindboxService.cs
+++ BlindTreasure.Application/Interfaces/IBlindboxService.cs
@@ -14,6 +14,5 @@ public interface IBlindBoxService
     Task<BlindBoxDetailDto> SubmitBlindBoxAsync(Guid blindBoxId);
     Task<BlindBoxDetailDto> ReviewBlindBoxAsync(Guid blindBoxId, bool approve, string? rejectReason = null);
     Task<BlindBoxDetailDto> ClearItemsFromBlindBoxAsync(Guid blindBoxId);
-
     Task<BlindBoxDetailDto> DeleteBlindBoxAsync(Guid blindBoxId);
 }
