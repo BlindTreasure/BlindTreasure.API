3ed57bf0f1c5ebb5ef37f77210acea4ac92bd333
Tiến Hoàng <minhtien11052003@gmail.com>
2025-08-04 14:55:25 +0700

    upgrade order-detail and checkout core

---
+++ BlindTreasure.Domain/Migrations/20250804072229_optimize_orderdetail.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlindTreasure.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class optimize_orderdetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalItemsShippingFee",
+                table: "OrderDetails");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DetailDiscountPromotion",
+                table: "OrderDetails",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "FinalDetailPrice",
+                table: "OrderDetails",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DetailDiscountPromotion",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "FinalDetailPrice",
+                table: "OrderDetails");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalItemsShippingFee",
+                table: "OrderDetails",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
