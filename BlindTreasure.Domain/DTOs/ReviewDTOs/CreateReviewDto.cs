9b11077f60fddcd589c828eba2f1368e224bc48a
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2025-08-10 18:24:19 +0700

    feat: Add default values for rating and comment

--- BlindTreasure.Domain/DTOs/ReviewDTOs/CreateReviewDto.cs
+++ BlindTreasure.Domain/DTOs/ReviewDTOs/CreateReviewDto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Microsoft.AspNetCore.Http;
 
 namespace BlindTreasure.Domain.DTOs.ReviewDTOs;
@@ -5,7 +6,9 @@ namespace BlindTreasure.Domain.DTOs.ReviewDTOs;
 public class CreateReviewDto
 {
     public Guid OrderDetailId { get; set; }
+    [DefaultValue(3)]
     public int Rating { get; set; }
+    [DefaultValue("sản phẩm chất lượng cao")]
     public string Comment { get; set; } = string.Empty;
     public List<IFormFile>? Images { get; set; } = new();
 }
