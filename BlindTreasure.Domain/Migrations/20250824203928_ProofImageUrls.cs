ca24fbe514ace9f531fae70fcfb3624902a218b4
Tiến Hoàng <minhtien11052003@gmail.com>
2025-08-25 04:01:19 +0700

    admin confirm image for payout

---
+++ BlindTreasure.Domain/Migrations/20250824203928_ProofImageUrls.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlindTreasure.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProofImageUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProofImageUrls",
+                table: "Payouts",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProofImageUrls",
+                table: "Payouts");
+        }
+    }
+}
