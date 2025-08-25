91748398620fc81045d64ced5fda280e6d2da9f9
Tiến Hoàng <minhtien11052003@gmail.com>
2025-08-26 00:52:51 +0700

    seller overview and fix few things

---
+++ BlindTreasure.Domain/DTOs/SellerDTOs/SellerOverviewDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlindTreasure.Domain.DTOs.SellerDTOs
+{
+    public class SellerOverviewDto
+    {
+        public Guid SellerId { get; set; }
+        public double AverageRating { get; set; }
+        public DateTime JoinedAt { get; set; }
+        public string? JoinedAtToText { get; set; }
+        public int ProductCount { get; set; }
+        public string? CompanyName { get; set; }
+        public string? CompanyArea { get; set; }
+    }
+}
